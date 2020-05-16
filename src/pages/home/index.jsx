1e64e26853316246de40c9873f2feabbec8a83a0
ferry5279 <2695506298@qq.com>
2020-05-16 18:02:37 +0800

    '开始home页面'

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
-
+import './style.less';
 const { SubMenu } = Menu;
 const { Header, Content, Sider } = Layout;
 
 // import axios from 'axios';
 // import {  message } from 'antd';
-// import './style.less';
+
 class index extends React.Component {
  
 
   render() {
-    return <div id=''  >
+    return <div id='home'  >
         <Layout>
     <Header className="header">
       <div className="logo" />
       <Menu
-        theme="dark"
+        theme="light"
         mode="horizontal"
         defaultSelectedKeys={['2']}
         style={{ lineHeight: '64px' }}
@@ -80,9 +80,9 @@ class index extends React.Component {
       </Sider>
       <Layout style={{ padding: '0 24px 24px' }}>
         <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          {/* <Breadcrumb.Item>Home</Breadcrumb.Item>
           <Breadcrumb.Item>List</Breadcrumb.Item>
-          <Breadcrumb.Item>App</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item> */}
         </Breadcrumb>
         <Content
           style={{
