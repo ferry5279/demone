be1c361115f2e5fd793ab6bcacc8165113903942
ferry5279 <2695506298@qq.com>
2020-05-17 15:29:02 +0800

    查找

--- src/pages/sample/index.jsx
+++ src/pages/sample/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 
 class index extends React.Component {
-
+  componentDidMount() {
+    console.log(this.props)
+   axios.get('https://blogs.zdldove.top/Home/Apis/listWithPage?page='+4).then(res=>{
+     console.log(res)
+    //  this.setState({
+    //    data:res.data.users
+    //  })
+   })
+  }
 
   render() {
     return <div>2222</div>;
