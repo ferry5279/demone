3d9b6c38573f84d3d81866dc7856e9b55138c778
ferry5279 <2695506298@qq.com>
2020-05-17 13:22:46 +0800

    开始table页面

--- src/pages/table/index.jsx
+++ src/pages/table/index.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
-// import { Form, Input, Button ,Upload,Icon,message} from 'antd';
+import {  Button ,message} from 'antd';
 import axios from 'axios';
-// import './style.less'
+import './style.less'
 
 class index extends React.Component {
- 
+  state={
+    data:[]
+  }
+  componentDidMount() {
+   axios.get('https://api.baxiaobu.com/index.php/home/v5/findUser').then(res=>{
+     console.log(res)
+     this.setState({
+       data:res.data.users
+     })
+   })
+ }
   render() {
-  
-    return <div id=''>
-      
+    const { data } = this.state;
+    return <div id='table'>
+      <h2>Striped Table</h2>
+      <ul id='ul'>
+        <li>Add  class</li>
+        <li>.table-striped</li>
+        <li><Button type='primary'>Add User</Button></li>
+      </ul>
     </div>;
   }
 }
