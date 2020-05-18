213cba872d1919b5cc82290e6efda07f6e9c73d2
ferry5279 <2695506298@qq.com>
2020-05-18 19:14:10 +0800

    整理index.js代码

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
-// import { Provider } from 'reasct-redux'
 import './index.less';
-// import '@/untils/rem.js'
 import '@/untils/reset.css'
 import Router from '@/router'
 ReactDOM.render(
